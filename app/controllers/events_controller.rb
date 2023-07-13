3a6d8a42ad00d2c757652e66b7c0fa03ebb1bdd3
Eduardo Yutaka Nakanishi <eduardo.nakanishi@gmail.com>
2023-07-13 17:55:53 -0300

    fix: failing tests

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,12 +6,12 @@ class EventsController < ApplicationController
     when 'deposit'
       @account = Account.find_or_create_by!(account_id: params[:destination])
       @account.update(balance: @account.balance + params[:amount])
-      render json: { destination: { id: @account.account_id, balance: @account.balance } }, status: :created
+      render json: { destination: { id: @account.account_id.to_s, balance: @account.balance } }, status: :created
     when 'withdraw'
       @account = Account.find_by(account_id: params[:origin])
       if @account
         @account.update(balance: @account.balance - params[:amount])
-        render json: { origin: { id: @account.account_id, balance: @account.balance } }, status: :created
+        render json: { origin: { id: @account.account_id.to_s, balance: @account.balance } }, status: :created
       else
         render json: 0, status: :not_found
       end
@@ -22,15 +22,15 @@ class EventsController < ApplicationController
       if @origin_account
         @origin_account.update(balance: @origin_account.balance - params[:amount])
         @destination_account.update(balance: @destination_account.balance + params[:amount])
-        render json: { origin: { id: @origin_account.id, balance: @origin_account.balance }, destination: { id: @destination_account.id, balance: @destination_account.balance } }, status: :created
+        render json: { origin: { id: @origin_account.account_id.to_s, balance: @origin_account.balance }, destination: { id: @destination_account.account_id.to_s, balance: @destination_account.balance } }, status: :created
       else
-        retrn json: 0, status: :not_found
+        render json: 0, status: :not_found
       end
     end
   end
 
   def reset
     Account.delete_all
-    render status: :ok
+    render json: 'OK', status: :ok
   end
 end
